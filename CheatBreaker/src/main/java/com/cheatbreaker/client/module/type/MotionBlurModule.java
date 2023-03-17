b8cb335405357558666540a9ebc6609d82707a2a
Ian <60456400+KaboomB52@users.noreply.github.com>
2023-03-17 11:44:25 -0500

    Update MotionBlurModule.java
    Reenable to begin testing

--- CheatBreaker/src/main/java/com/cheatbreaker/client/module/type/MotionBlurModule.java
+++ CheatBreaker/src/main/java/com/cheatbreaker/client/module/type/MotionBlurModule.java
@@ -23,40 +23,40 @@ public class MotionBlurModule extends AbstractModule {
     }
 
     public void bindShader() {
-//        if (OpenGlHelper.isFramebufferEnabled() && OpenGlHelper.shadersSupported) {
-//            if (minecraft.entityRenderer.theShaderGroup != null) {
-//                minecraft.entityRenderer.theShaderGroup.deleteShaderGroup();
-//            }
-//            try {
-//                minecraft.entityRenderer.theShaderGroup = new ShaderGroup(minecraft.getTextureManager(), minecraft.entityRenderer.resourceManager, minecraft.getFramebuffer(), new ResourceLocation("shaders/post/motionblur.json"));
-//                minecraft.entityRenderer.theShaderGroup.createBindFramebuffers(minecraft.displayWidth, minecraft.displayHeight);
-//            }
-//            catch (Exception exception) {
-//                // empty catch block
-//            }
-//        }
+        if (OpenGlHelper.isFramebufferEnabled() && OpenGlHelper.shadersSupported) {
+            if (minecraft.entityRenderer.theShaderGroup != null) {
+                minecraft.entityRenderer.theShaderGroup.deleteShaderGroup();
+            }
+            try {
+                minecraft.entityRenderer.theShaderGroup = new ShaderGroup(minecraft.getTextureManager(), minecraft.entityRenderer.resourceManager, minecraft.getFramebuffer(), new ResourceLocation("shaders/post/motionblur.json"));
+                minecraft.entityRenderer.theShaderGroup.createBindFramebuffers(minecraft.displayWidth, minecraft.displayHeight);
+            }
+            catch (Exception exception) {
+                // empty catch block
+            }
+        }
     }
 
     private void drawShader() {
-//        bindShader();
-//        ShaderGroup shaderGroup = minecraft.entityRenderer.getShaderGroup();
-//        try {
-//            if (this.minecraft.entityRenderer.isShaderActive() && this.minecraft.thePlayer != null) {
-//                for (Shader shader : shaderGroup.listShaders) {
-//                    ShaderUniform uniform = shader.getShaderManager().func_147991_a("Phosphor");
-//                    if (uniform == null) continue;
-//                    float f = 1.028125f * 0.68085104f + (float) (Integer) this.amount.getValue() / (float)100 * (float)3 - 0.7f * 0.014285714f;
-//                    int n = this.color.getColorValue();
-//                    float f2 = (float)(n >> 16 & 0xFF) / (float)255;
-//                    float f3 = (float)(n >> 8 & 0xFF) / (float)255;
-//                    float f4 = (float)(n & 0xFF) / (float)255;
-//                    uniform.func_148095_a(f * f2, f * f3, f * f4);
-//                }
-//            }
-//        }
-//        catch (IllegalArgumentException illegalArgumentException) {
-//            Throwables.propagate(illegalArgumentException);
-//        }
+        bindShader();
+        ShaderGroup shaderGroup = minecraft.entityRenderer.getShaderGroup();
+        try {
+            if (this.minecraft.entityRenderer.isShaderActive() && this.minecraft.thePlayer != null) {
+                for (Shader shader : shaderGroup.listShaders) {
+                    ShaderUniform uniform = shader.getShaderManager().func_147991_a("Phosphor");
+                    if (uniform == null) continue;
+                    float f = 1.028125f * 0.68085104f + (float) (Integer) this.amount.getValue() / (float)100 * (float)3 - 0.7f * 0.014285714f;
+                    int n = this.color.getColorValue();
+                    float f2 = (float)(n >> 16 & 0xFF) / (float)255;
+                    float f3 = (float)(n >> 8 & 0xFF) / (float)255;
+                    float f4 = (float)(n & 0xFF) / (float)255;
+                    uniform.func_148095_a(f * f2, f * f3, f * f4);
+                }
+            }
+        }
+        catch (IllegalArgumentException illegalArgumentException) {
+            Throwables.propagate(illegalArgumentException);
+        }
     }
 
 }
